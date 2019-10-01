3f0da265a64f35c558813c0c7b67946ab230931e
Melanie <melaniecurrie01@gmail.com>
2019-10-01 01:17:46 +0000

    created function to generate short urls ... not sure how to impliment

--- express_server.js
+++ express_server.js
@@ -1,13 +1,28 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = (num) => {
+  let output = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let charLength = characters.length;
+  for (let i = 0; i < num; i++) {
+    output += characters.charAt(Math.floor(Math.random() * charLength));
+  }
+  return output;
+};
+// generateRandomString(6);
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -38,3 +53,10 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
